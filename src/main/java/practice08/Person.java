9fbbcc53cd3db0a0ea9071e7a21cdb5c91668b7f
qinshuqian <524477767@qq.com>
2019-04-02 18:26:38 +0800

    modify practice08

--- src/main/java/practice08/Person.java
+++ src/main/java/practice08/Person.java
@@ -3,12 +3,25 @@ package practice08;
 public class Person extends practice01.Person{
     private int id;
 
-    public Person(int id,String name, int age) {
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Person(int id, String name, int age) {
         super(name, age);
         this.id = id;
     }
 
     public Person(String name, int age) {
         super(name, age);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj) || ((obj instanceof Person) && (((Person) obj).getId() == getId()));
     }
 }
